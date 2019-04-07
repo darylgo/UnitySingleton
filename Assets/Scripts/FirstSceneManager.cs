41499002cd46a2f15764d22f134983d8f0e18e33
DarylGo <hjpdyxhjd@gmail.com>
2019-04-07 21:43:47 +0800

    提交项目

---
+++ Assets/Scripts/FirstSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FirstSceneManager : MonoBehaviour
+{
+    public Text valueText;
+
+    private void Start()
+    {
+        valueText.text = GameManager.Instance.Value.ToString();
+    }
+
+    public void OnButtonClick()
+    {
+        GameManager.Instance.Value++;
+        SceneManager.LoadScene("SecondScene");
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public int Value { get; set; } = 0;
+}
